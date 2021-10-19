ec40ef78871821bad01adcde57fb39d7f32ff4ca
edgariphdz98 <edgariphdz98@tamu.edu>
2021-10-19 13:05:45 -0500

    Addressed feedback, cleanup

--- src/rqt_mypkg/src/rqt_mypkg/components/BagPlayer.py
+++ src/rqt_mypkg/src/rqt_mypkg/components/BagPlayer.py
@@ -1,13 +1,10 @@
-from logging import error
-import os
-import rospy
-import rosbag
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget, QVBoxLayout, QPushButton, QSlider, QHBoxLayout
 from python_qt_binding.QtCore import Qt
 from sensor_msgs.msg import PointCloud2
+import rospy
 
-# encapsulation of a bag player 
+# encapsulation of a bag player
 class BagPlayer(QWidget):
 
     # rvizTopic is the topic where pc2 messages are send to display on the rviz main frame
@@ -19,7 +16,7 @@ class BagPlayer(QWidget):
         # the bag containing the messages and frames of Frame class as well as current frame number
         self.bag = None
         self.frames = []
-        self.pointCloudTopic  = pointCloudTopic
+        self.pointCloudTopic = pointCloudTopic
         self.currentFrame = -1
 
         # ui components references
@@ -30,16 +27,17 @@ class BagPlayer(QWidget):
         # loading ui
         self.initUI()
 
-        # setting up publisher 
+        # setting up publisher
         self.setUpPublisher(rvizTopic)
 
     # updates a bag with the given bag and frames
-    def updateBag(self,_bag, _frames):
+    def updateBag(self, _bag, _frames):
         self.bag = _bag
-        self.frames  = _frames
+        self.frames = _frames
         self.currentFrame = 0
-        self.slider.setMaximum( len(self.frames)-1)
-        rospy.loginfo("Rosbag updated, Number of Frames: {} ".format(len(self.frames)))
+        self.slider.setMaximum(len(self.frames)-1)
+        rospy.loginfo(
+            "Rosbag updated, Number of Frames: {} ".format(len(self.frames)))
         # publish first frame
         self.publishFrame(self.currentFrame)
 
@@ -50,14 +48,15 @@ class BagPlayer(QWidget):
 
         # slider  attributes
         self.slider = QSlider(Qt.Horizontal)
-        self.slider.setTracking( True )
-        self.slider.setMinimum( 1 )
-        self.slider.setMaximum( len(self.frames))
+        self.slider.setTracking(True)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(len(self.frames))
         self.slider.valueChanged.connect(self.sliderOnChange)
 
         # button layout
         buttonBox = QHBoxLayout()
-        self.nextButton, self.prevButton = QPushButton("Next"), QPushButton("Previous")
+        self.nextButton, self.prevButton = QPushButton(
+            "Next"), QPushButton("Previous")
         self.nextButton.clicked.connect(self.playNext)
         self.prevButton.clicked.connect(self.playPrev)
         buttonBox.addWidget(self.prevButton)
@@ -71,32 +70,34 @@ class BagPlayer(QWidget):
     # setup publisher to publish point cloud messages
     def setUpPublisher(self, topic):
         try:
-            self.bagPublisher = rospy.Publisher(topic,PointCloud2,queue_size=1)
-        except error:
+            self.bagPublisher = rospy.Publisher(
+                topic, PointCloud2, queue_size=1)
+        except Exception as error:
             rospy.logerr(error)
-    
+
     # plays the next frame in the bag and sets the slider on that index
     def playPrev(self):
         if len(self.frames) > 1 and self.currentFrame > 0:
-            self.currentFrame  -= 1
+            self.currentFrame -= 1
             self.slider.setValue(self.currentFrame)
-    
+
     # plays the previous frame in the bag and sets the slider on that index
     def playNext(self):
-        if len(self.frames) > 1 and self.currentFrame < len(self.frames) -1:
-            self.currentFrame  += 1
+        if len(self.frames) > 1 and self.currentFrame < len(self.frames) - 1:
+            self.currentFrame += 1
             self.slider.setValue(self.currentFrame)
 
     # publish the message from a frame index in our frame list and publishing to the rviz topic to display it
-    def publishFrame(self,frameNumber):
+    def publishFrame(self, frameNumber):
         # timestamp associated the message to read
         frameTimeStamp = self.frames[frameNumber].timestamp
         publishingMSG = None
-        # getting pointcloud message 
-        for topic, msg, t  in self.bag.read_messages(self.pointCloudTopic,start_time=frameTimeStamp,end_time=frameTimeStamp):
+        # getting pointcloud message
+        for topic, msg, t in self.bag.read_messages(self.pointCloudTopic, start_time=frameTimeStamp, end_time=frameTimeStamp):
             publishingMSG = msg
         self.bagPublisher.publish(publishingMSG)
-        rospy.loginfo("Current Frame {}, timestamp: {}".format(frameNumber,frameTimeStamp))
+        rospy.loginfo("Current Frame {}, timestamp: {}".format(
+            frameNumber, frameTimeStamp))
 
     # publishes a frame anytime the slider changes
     def sliderOnChange(self, newFrame):
